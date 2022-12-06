bf6141634cc61d303296ee7f87f032f506222803
NabilaSam <nabzluciano@gmail.com>
2022-12-06 16:31:01 +0200

    index.html

--- style.js
+++ style.js
@@ -1,2 +1,27 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarlinks = document.getElementsByClassName('navbar-links')[0]
+// // Nav bar
+// const toggleButton = document.getElementsByClassName('toggle-button')[0]
+// const navbarlinks = document.getElementsByClassName('navbar-links')[0]
+
+// // In the event where the togglebutton is clicked, the navbar links will display
+// toggleButton.addEventListener('click', ()=>{
+//     navbarlinks.classList.toggle('active')
+// })
+
+// filter option
+filterCard("all") function filterCard...filterCard
+
+
+
+// Admin page
+fetch('./catalogue.json')
+.then((res)=>{
+    // use the .json method to convert the json data to a js object
+    return res.json();
+})
+// (function as argument) now fetch the products
+.then((catalogue)=>{
+    // use the id added to the table on admin page
+    let cat = document.querySelector("#catalogue");
+    // set its value to an empty string
+    let displaycat = "";
+})
