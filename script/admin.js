f321eadfc0aed5fe6244451bd51dd3efcfeb4eaf
NabilaSam <nabzluciano@gmail.com>
2022-12-08 16:21:26 +0200

    Thurs afternoon

--- script/admin.js
+++ script/admin.js
@@ -1,44 +1,42 @@
 // let all = JSON.parse(localStorage.getItem('all'));
 
 let tbody = document.getElementById('catalogue');
+// Table
+let catalogue= JSON.parse(localStorage.catalogue)
 
-async function fetchData(){
-    let res =await fetch("./catalogue.json");
-    let data = await res.json();
-    return data.all;
-};
-async function display(){
-    let data = await fetchData();
-    data.forEach(item => {
-        tbody.innerHTML += `
+    catalogue.forEach(item => {
+        try {
+            tbody.innerHTML += `
         <tr>
-          <th scope="row">${item.id}</th>
+          <th scope="row"><img src="${item.image}" style="width: 40px;"></th>
           <td>${item.name}</td>
           <td> ${item.price}</td>
           <td> ${item.style}</td>
           <td> <button class="btn btn-secondary btn-lg" style="background-color: rgb(181, 218, 181); color: black;" id="Editbtn">Edit</button>
           <button id="dltbtn" class="btn btn-secondary btn-lg" style="background-color: rgb(181, 218, 181); color: black;">Delete</button></td>
       </tr> `
-});
-};
+
+        } catch (error) {
+            console.log(error);
+        }
+     } );  
  
-display();
 
-// Table
 
-// function display(){
-    
-//         console.log(all);
-//         tbody.innerHTML ="";
-//         all.forEach(item => {
-//             tbody.innerHTML += `
-//             <tr>
-//               <td> <img src="${item.Image}" width="30px" alt="pics"></td>
-//               <td> ${item.id}></td>
-//               <td> ${item.price}></td>
-//               <td> ${item.Style}></td>
-//           </tr> `
-//         });
-    
-// };
-// display();
+// Add button
+let add = document.querySelector('#addBtn')
+add.addEventListener('click', (e)=>{
+    e.preventDefault();
+    let name =  document.querySelector('#addName').value;
+    let price = document.querySelector('#addPrice').value;
+    let style = document.querySelector('#addStyle').value;
+
+    catalogue.push({
+        name,
+        price,
+        style
+    })
+    console.log(catalogue.at(-1))
+    localStorage.setItem('catalogue', JSON.stringify(catalogue));
+
+})
--- script/script.js
+++ script/script.js
@@ -1,131 +1,124 @@
-// // Product data
-// let all = [
-//     {
-//         "id": 1,
-//         "Image": "./imgs/54279e55845716e115af9f45943ec0bc.jpg",
-//         "price": "R300",
-//         "Style": "ethereal"
+// Product data
+// stringify will convert the objects to a string in the localstorage
 
-//     },
-//     {
-//         "id": 2,
-//         "Image": "./imgs/416d7a5f78ace1f15cae43d22f4c04fc.jpg",
-//         "price": "R300",
-//         "Style": "ethereal"
+localStorage.setItem('catalogue', JSON.stringify([
+    {
+        "id": 1,
+        "name": "Black lace corset",
+        "image": "../imgs/54279e55845716e115af9f45943ec0bc.jpg",
+        "price": "300",
+        "style": "ethereal"
+    },
+    {
+        "id": 2,
+        "name": "Floral lace corset",
+        "image": "../imgs/416d7a5f78ace1f15cae43d22f4c04fc.jpg",
+        "price": "420",
+        "style": "ethereal"
+    },
+    {
+        "id": 3,
+        "name": "Black steampunk corset",
+        "image": "../imgs/blksteampunk.webp",
+        "price": "500",
+        "style": "steampunk"
+    },
+    {
+        "id": 4,
+        "name": "Brown steampunk corset",
+        "image": "../imgs/brwnsteampunk.webp",
+        "price": "500",
+        "style": "steampunk"
+    },
+    {
+        "id": 5,
+        "name": "Crimson red classic corset",
+        "image": "../imgs/redcorset.webp",
+        "price": "450",
+        "style": "classic"
+    },
+    {
+        "id": 6,
+        "name": "Black and gold corset",
+        "image": "../imgs/black&goldcorset.webp",
+        "price": "380",
+        "style": "classic"
 
-//     },
-//     {
-//         "id": 3,
-//         "Image": "./imgs/blksteampunk.webp",
-//         "price": "R400",
-//         "Style": "steampunk"
+    },
+    {
+        "id": 7,
+        "name": "White floral casual",
+        "image": "../imgs/lace-up-floral-corset-top-anotherchill-1_800x.webp",
+        "price": "320",
+        "style": "casual"
 
-//     },
-//     {
-//         "id": 4,
-//         "Image": "./imgs/brwnsteampunk.webp",
-//         "price": "R350",
-//         "Style": "steampunk"
+    },
+    {
+        "id": 8,
+        "name": "cottagecore corset with sleeves",
+        "image": "../imgs/cottagecore.webp",
+        "price": "300",
+        "style": "casual"
 
-//     },
-//     {
-//         "id": 5,
-//         "Image": "./imgs/redcorset.webp",
-//         "price": "R420",
-//         "Style": "classic"
+    },
+    {
+        "id": 9,
+        "name": "Black & blue corset with sleeves",
+        "image": "../imgs/black&bluecorsetwlacesleeves.webp",
+        "price": "250",
+        "style": "sleeves"
 
-//     },
-//     {
-//         "id": 6,
-//         "Image": "./imgs/black&goldcorset.webp",
-//         "price": "R300",
-//         "Style": "classic"
+    },
+    {
+        "id": 10,
+        "name": "White & gold corset",
+        "image": "../imgs/goldcorsetwlacesleeves.webp",
+        "price": "200",
+        "style": "sleeves"
 
-//     },
-//     {
-//         "id": 7,
-//         "Image": "./imgs/lace-up-floral-corset-top-anotherchill-1_800x.webp",
-//         "price": "R300",
-//         "Style": "casual"
+    },
+]));
 
-//     },
-//     {
-//         "id": 8,
-//         "Image": "./imgs/cottagecore.webp",
-//         "price": "R300",
-//         "Style": "casual"
+// getItem = fetches data from local storage
+// JSON.parse = convert the data to a JSON data in the local storage
+let x = JSON.parse(localStorage.getItem('catalogue'));
 
-//     },
-//     {
-//         "id": 9,
-//         "Image": "./imgs/blkcasual.webp",
-//         "price": "R300",
-//         "Style": "casual"
+console.log(x);
 
-//     },
-//     {
-//         "id": 10,
-//         "Image": "./imgs/black&bluecorsetwlacesleeves.webp",
-//         "price": "R300",
-//         "Style": "sleeves"
-
-//     },
-//     {
-//         "id": 11,
-//         "Image": "./imgs/goldcorsetwlacesleeves.webp",
-//         "price": "R300",
-//         "Style": "sleeves"
-
-//     },
-//     {
-//         "id": 12,
-//         "Image": "./imgs/black&bluecorsetwlacesleeves.webp",
-//         "price": "R500",
-//         "Style": "sleeves"
-
-//     }
-// ]
-
-// Fetch API
-async function fetchData(){
-    let res = await fetch('./catalogue.json');
-    let data = await res.json();
-    return data.books;
-};
+let products = JSON.parse(localStorage.getItem('catalogue'))? 
+JSON.parse(localStorage.getItem('catalogue')):
+JSON.parse(localStorage.setItem("catalogue", JSON.stringify(catalogue)));
 
 // product list in cards
-let cards = document.getElementById('cards');
+let pCards = document.getElementById('cards');
 
-async function display(){
-    let data = await fetchData();
-    data.forEach((item)=>{
-        cards.innerHTML += `
-        <div class="card" style="width: 17rem;">
-                    <img src="${item.image}" style="width:fit-content;">
+function display(){
+    try {
+        products.forEach((item)=>{
+        pCards.innerHTML += `
+        <div class="card col-3" style="width: 17rem;">
+                    <img src="${item.image}">
                 <div class="card-body">
                     <h3 class="card-title"> ${item.name}</h3>
                     <p><b>Style: ${item.style}</b></p>
                     <h5>Price: R ${item.price}</h5>
                     <button class="btn btn-outline-dark" style="background-color: rgb(222, 101, 178);">Add to checkout</button>
                 </div>
-            </div>`
-    })
+            </div>` })
+    } catch (error) {
+        console.log(error);
+    }
 }
 display();
 
 // syntax for a conditional operator( conditional ? expression_when_true : expression_when_false;)
 // Sample Data
-// Using stringify to adjust the data in the ideal form for this js file
-let products = JSON.parse(localStorage.getItem('all')) ?
-JSON.parse(localStorage.getItem('all')):
-JSON.parse(localStorage.setItem("all", JSON.stringify(all)));
+// Using stringify to adjust the data in the ideal form for this js file. chrck local storage in browser
+// let products = JSON.parse(localStorage.getItem('all')) ?
+// JSON.parse(localStorage.getItem('all')):
+// JSON.parse(localStorage.setItem("all", JSON.stringify(all)));
 
 
-
-
-
-
-// Admin
 
 
 // ...
